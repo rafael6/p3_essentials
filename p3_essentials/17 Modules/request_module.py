d99d977f1f101e41abfe6c86ec9ebc02e13e9d8e
Rafael Gonzalez Morales <rafael.gm01@gmail.com>
2024-03-03 12:53:13 -0500

    Update request_module.py

--- p3_essentials/17 Modules/request_module.py
+++ p3_essentials/17 Modules/request_module.py
@@ -1,9 +1,21 @@
 __author__ = 'rafael'
 
 import requests
+import json
 
 r = requests.get('http://httpbin.org/get')
 print(r.status_code)
 print(r.headers['content-type'])
 print(r.encoding)
 print(r.json())
+
+# This will return a str
+response = requests.get(uri)
+print(response.text)
+
+# This will retrun a Python collection dict in this example
+# Method load decodes json to Python
+# See json file for details
+response = requests.get(uri)
+response_json = json.loads(response.text)
+print(response_json)
